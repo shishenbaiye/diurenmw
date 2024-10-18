b3ef16ed46e0ee1fc78a3aa03f2321d128ec35e9
zebin.ge <zebin.ge@appshahe.com>
2024-10-18 17:38:32 +0800

    other：暂存一部分技能模块修改

--- diurenmw/JavaScripts/modules/skillModule/PlayerSkillScrpit.ts
+++ diurenmw/JavaScripts/modules/skillModule/PlayerSkillScrpit.ts
@@ -1,12 +1,55 @@
 import { Constructor } from "../../framework/DI/MContainer";
+import { AbilitySystemComponent } from "../gasModule/gameAbilitys/ASC/AbilitySystemComponent";
 import { GameAbility } from "../gasModule/gameAbilitys/GA/GameAbility";
 
 @Component
 export default class PlayerSkillScrpit extends Script{
 
-    skill1: Constructor<GameAbility>
-    skill2: Constructor<GameAbility>
-    skill3: Constructor<GameAbility>
-    skill4: Constructor<GameAbility>
-    
+    normalAttack: string[] = []
+    skill1: string = ""
+    skill2: string = ""
+    skill3: string = ""
+    skill4: string = ""
+    ownerAsc: AbilitySystemComponent;
+    setSkill(skill: string, index: number){
+        switch(index){
+            case 0:
+                this.skill1 = skill;
+                break;
+            case 1:
+                this.skill2 = skill;
+                break;
+            case 2:
+                this.skill3 = skill;
+                break;
+            case 3:
+                this.skill4 = skill;
+                break;
+        }
+    }
+
+    setNormalAttack(skill: string[]){
+        this.normalAttack = skill;
+    }
+
+    activeNormalAttack(){
+        
+    }
+
+    activeSkill(index: number){
+        switch(index){
+            case 0:
+                this.ownerAsc.tryActiveGameAbilityByTag(this.skill1);
+                break;
+            case 1:
+                this.ownerAsc.tryActiveGameAbilityByTag(this.skill2);
+                break;
+            case 2:
+                this.ownerAsc.tryActiveGameAbilityByTag(this.skill3);
+                break;
+            case 3:
+                this.ownerAsc.tryActiveGameAbilityByTag(this.skill4);
+                break;
+        }
+    }
 }
--- diurenmw/JavaScripts/modules/skillModule/SkillModuleS.ts
+++ diurenmw/JavaScripts/modules/skillModule/SkillModuleS.ts
@@ -1,4 +1,7 @@
 import { GameEventBus } from "../../common/eventBus/EventBus";
+import { AbilitySystemComponent } from "../gasModule/gameAbilitys/ASC/AbilitySystemComponent";
+import PlayerSkillScrpit from "./PlayerSkillScrpit";
+import { SkillManager } from "./SkillManager";
 import { SkillModuleC } from "./SkillModuleC";
 import { SkillModuleData } from "./SkillModuleData";
 
@@ -8,6 +11,53 @@ export class SkillModuleS extends ModuleS<SkillModuleC,SkillModuleData>{
     }
 
     onAttributeAllReady(player: mw.Player) {
-        
+        let skillScript = player.character.addComponent(PlayerSkillScrpit);
+        if(skillScript){
+            let asc = player.character.getComponent(AbilitySystemComponent);
+            if(!asc) {
+                console.error(`玩家${player.userId}asc组件没准备好，添加技能脚本失败`);
+                return;
+            }
+            skillScript.ownerAsc = asc;
+            let skillData = this.getPlayerData(player);
+            skillData.haveSkills.forEach((skill)=>{
+                let res = SkillManager.instance.getSkillByName(skill);
+                if(res){
+                    asc.giveAbility(res);
+                }
+            })
+
+            if(skillData.normalSkillList.length > 0){
+                skillScript.normalAttack = skillData.normalSkillList;
+            }
+
+            if(skillData.skill1){
+                skillScript.skill1 = skillData.skill1;
+            }
+
+            if(skillData.skill2){
+                skillScript.skill2 = skillData.skill2;
+            }
+
+            if(skillData.skill3){
+                skillScript.skill3 = skillData.skill3;
+            }
+
+            if(skillData.skill4){
+                skillScript.skill4 = skillData.skill4;
+            }
+
+        }
+    }
+
+    addSkill(player: mw.Player, skillName: string){
+        let skillData = this.getPlayerData(player);
+        let res = SkillManager.instance.getSkillByName(skillName);
+        if(res){
+            let asc = player.character.getComponent(AbilitySystemComponent);
+            asc.giveAbility(res);
+            skillData.haveSkills.push(skillName);
+            skillData.save(true);
+        }
     }
 }
