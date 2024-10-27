6a116f5374c237141bf896e3d9e7886a2046107b
LIULEI <liuhuanqiang@appshahe.com>
2024-10-28 01:24:39 +0800

    add:添加Gm

--- diurenmw/JavaScripts/modules/npcModule/behavior/Behavior.ts
+++ diurenmw/JavaScripts/modules/npcModule/behavior/Behavior.ts
@@ -1,44 +1,43 @@
-import { IBehavior } from "../interface/IBehavior";
-import { BehaviorController } from "./BehaviorController";
+// import { IBehavior } from "../interface/IBehavior";
 
-export abstract class Behavior implements IBehavior {
+// export abstract class Behavior implements IBehavior {
 
-    public controller: BehaviorController
+//     // public controller: BehaviorController
 
-    constructor(public owner: INpcClientCtrlBase) { }
+//     // constructor(public owner: INpcClientCtrlBase) { }
 
-    abstract checkCondition(dt: number): boolean;
+//     // abstract checkCondition(dt: number): boolean;
 
-    abstract onReset()
+//     // abstract onReset()
 
-    public setFollow(isFollow: boolean, stopDistance: number = 30) {
-        if (isFollow == this.controller.isInFollowing) {
-            return;
-        }
-        if (isFollow) {
-            this.owner.npcCom.startFollow(stopDistance);
-        }
-        else {
-            // console.log("stopFollow")
-            this.owner.npcCom.stopFollow()
-        }
-        this.controller.isInFollowing = isFollow;
-    }
+//     // public setFollow(isFollow: boolean, stopDistance: number = 30) {
+//     //     if (isFollow == this.controller.isInFollowing) {
+//     //         return;
+//     //     }
+//     //     if (isFollow) {
+//     //         this.owner.npcCom.startFollow(stopDistance);
+//     //     }
+//     //     else {
+//     //         // console.log("stopFollow")
+//     //         this.owner.npcCom.stopFollow()
+//     //     }
+//     //     this.controller.isInFollowing = isFollow;
+//     // }
 
-    /**
-     * 
-     * @param exitType 
-     * @returns 
-     */
-    protected checkExitCd(exitType: NpcExitType): boolean {
-        if (this.controller.exitCdMap.has(exitType)) {
-            //console.log(exitType + "exitType" + this.controller.exitCdMap.get(exitType))
-            return this.controller.exitCdMap.get(exitType) <= 0;
-        }
-        else {
-            return true;
-        }
-    }
+//     // /**
+//     //  *
+//     //  * @param exitType
+//     //  * @returns
+//     //  */
+//     // protected checkExitCd(exitType: NpcExitType): boolean {
+//     //     if (this.controller.exitCdMap.has(exitType)) {
+//     //         //console.log(exitType + "exitType" + this.controller.exitCdMap.get(exitType))
+//     //         return this.controller.exitCdMap.get(exitType) <= 0;
+//     //     }
+//     //     else {
+//     //         return true;
+//     //     }
+//     // }
 
 
-}
+// }
---
+++ diurenmw/JavaScripts/ui-generate/Gm/GMItem_generate.ts
@@ -0,0 +1,59 @@
+﻿
+/**
+ * AUTO GENERATE BY UI EDITOR.
+ * WARNING: DO NOT MODIFY THIS FILE,MAY CAUSE CODE LOST.
+ * ATTENTION: onStart 等UI脚本自带函数不可改写为异步执行，有需求的异步逻辑请使用函数封装，通过函数接口在内部使用
+ * UI: UI/Gm/GMItem.ui
+*/
+
+
+
+@UIBind('UI/Gm/GMItem.ui')
+export default class GMItem_Generate extends UIScript {
+		private button_Internal: mw.StaleButton
+	public get button(): mw.StaleButton {
+		if(!this.button_Internal&&this.uiWidgetBase) {
+			this.button_Internal = this.uiWidgetBase.findChildByPath('MWCanvas_2147482460/button') as mw.StaleButton
+		}
+		return this.button_Internal
+	}
+
+
+ 
+	/**
+	* onStart 之前触发一次
+	*/
+	protected onAwake() {
+		this.canUpdate = false;
+		this.layer = UILayerMiddle;
+        this.initButtons();
+	}
+
+	protected initButtons() {
+		//按钮添加点击
+		
+		this.initLanguage(this.button);
+		this.button.touchMethod = (ButtonTouchMethod.PreciseTap);
+		
+	
+		//按钮添加点击
+		
+
+		//按钮多语言
+		
+		//文本多语言
+		
+		//文本多语言
+		
+
+	}
+
+	private initLanguage(ui: StaleButton | TextBlock) {
+        let call = UIScript.getBehavior("lan");
+        if (call && ui) {
+            call(ui);
+        }
+    }
+	 
+}
+ 
